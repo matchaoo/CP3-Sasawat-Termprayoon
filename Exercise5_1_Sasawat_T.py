1ff706c7161db86b0f89dcecee4e383b31e6c83e
matchaoo <sasawat.term@gmail.com>
2022-07-06 00:51:59 +0700

    Create Number Calculator program

---
+++ Exercise5_1_Sasawat_T.py
@@ -0,0 +1,11 @@
+num1 = int(input("1st Number: "))
+num2 = int(input("2nd Number: "))
+plus = num1 + num2
+minus = num1 - num2
+multiply = num1*num2
+divide = int(num1/num2)
+
+print(num1, "+", num2, "=", plus)
+print(num1, "-", num2, "=", minus)
+print(num1, "*", num2, "=", multiply)
+print(num1, "/", num2, "=", divide)
