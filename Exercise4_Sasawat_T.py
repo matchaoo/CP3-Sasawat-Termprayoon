b04fbe4b3b73191986ca228765dcb2cca14427c0
matchaoo <sasawat.term@gmail.com>
2022-07-05 15:19:40 +0700

    Create varible

---
+++ Exercise4_Sasawat_T.py
@@ -0,0 +1,11 @@
+#ตัวแปรสำหรับเก็บข้อมูลคะแนนในรายวิชาต่างๆ
+FE = 82.5
+GB = 77.5
+ICS = 91.5
+CP = 87.5
+
+print("--- Your Score ---")
+print("Foundation English: ", FE)
+print("General Business: ", GB)
+print("Introduction to Computer System: ", ICS)
+print("Computer Programming: ", CP)
