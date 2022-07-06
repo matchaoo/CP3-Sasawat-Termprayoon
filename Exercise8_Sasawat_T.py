ecacbd74ddf9424aa0f7010f11ef8de807b61019
matchaoo <sasawat.term@gmail.com>
2022-07-06 17:12:51 +0700

    username_and_password
    fill username and password, and choose category

---
+++ Exercise8_Sasawat_T.py
@@ -0,0 +1,56 @@
+username_input = input("Username: ")
+password_input = input("Password: ")
+
+if username_input == "customer1" and password_input == "1234":
+    print("---------------------")
+    print("Welcome to Aigoo shop")
+    print("Please select category")
+    print(">>>")
+    print("1. Animals Doll")
+    print("2. Fruit Doll")
+    print(">>>")
+
+    select_category = int(input("Select Category Number: "))
+    if select_category == 1:
+        print("Item list")
+        print("1. Rabbit doll   159 THB")
+        print("2. Puppy doll    129 THB")
+        print("3. Fish doll     109 THB")
+        print("4. Tiger doll    169 THB")
+        print("5. Parrot doll   169 THB")
+        print(">>>")
+
+        doll_select = int(input("Please select your item: "))
+        amount = int(input("Amount: "))
+        print("------------------------")
+        if doll_select == 1:
+            total = amount*159
+            print("Total", total, "THB")
+
+        elif doll_select == 2:
+            total = amount*129
+            print("Total", total, "THB")
+
+        elif doll_select == 3:
+            total = amount*109
+            print("Total", total, "THB")
+
+        elif doll_select == 4:
+            total = amount*169
+            print("Total", total, "THB")
+
+        elif doll_select == 5:
+            total = amount*169
+            print("Total", total, "THB")
+
+    elif select_category == 2:
+        print("Item list")
+        print("1. Banana doll    89 THB")
+        print("2. Apple doll     99 THB")
+        print("3. Cherry doll   109 THB")
+        print("4. Coconut doll   89 THB")
+        print("5. Grape doll     99 THB")
+
+else:
+    print("Incorect username or password")
+    print("Try again")
