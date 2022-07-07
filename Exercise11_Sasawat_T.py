7571938d7582217e837a600647df2bc1c3f8e114
matchaoo <sasawat.term@gmail.com>
2022-07-07 17:46:39 +0700

    Answer2

--- Exercise11_Sasawat_T.py
+++ Exercise11_Sasawat_T.py
@@ -2,3 +2,11 @@
 number = int(input("Number: "))
 for x in range(number):
     print(" "*(number-(x-1)) + "*"*int((x+1)+x))
+
+#Answer2
+number = int(input("Number: "))
+for x in range(number):
+    star = ""
+    for y in range((x+1)+x):
+        star += "*"
+    print(" "*(number-(x-1)) + star)
